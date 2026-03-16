46504321707f08f30cb0a0130773337aa55b1547
Phạm Xuân Nguyên <xuannguyen2152005@gmail.com>
2026-03-16 21:17:32 +0700

    feat: add package update and delete APIs

--- src/controllers/package.controller.js
+++ src/controllers/package.controller.js
@@ -1,4 +1,7 @@
-const {createPackageService, getPackagesByGigIdService} = require("../services/package.service")
+const {createPackageService, 
+        getPackagesByGigIdService, 
+        updatePackageService, 
+        deletePackageService} = require("../services/package.service")
 
 const createPackage = async(req, res) => {
     try {
@@ -30,4 +33,37 @@ const getPackagesByGigId = async(req, res) => {
     }
 }
 
-module.exports = {createPackage, getPackagesByGigId}
+const updatePackage = async(req, res) => {
+    try {
+        const packageId = +req.params.id
+        const userId = req.user.userID
+
+        const pkg = await updatePackageService(packageId, userId, req.body)
+
+        res.status(200).json(pkg)
+
+    } catch (error) {
+        res.status(error.statusCode || 500).json({
+            message: error.message
+        })
+    }
+}
+
+const deletePackage = async(req, res) => {
+    try {
+        const packageId = +req.params.id
+        const userId = req.user.userID
+
+        const pkg = await deletePackageService(packageId, userId)
+
+        res.status(200).json(pkg)
+
+    } catch (error) {
+        res.status(error.statusCode || 500).json({
+            message: error.message
+        })
+    }
+}
+
+
+module.exports = {createPackage, getPackagesByGigId, updatePackage, deletePackage}
